228342871df845aeb0f1c6ac3ee328689002893a
Takanori Onuma <s1000607@sendai-nct.jp>
2015-04-07 13:01:33 +0900

    検索結果の表示
    検索してでたTODOデータを表示した。
    何もなければエラーを表示させている。
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- step2/todoProject/public/javascripts/search.js
+++ step2/todoProject/public/javascripts/search.js
@@ -6,7 +6,24 @@ $('#fm').submit(function() {
 function postList() {
   var text = $('#text').val();
 
-  $.get('/todoSearch', {word: text}, function(todos) {
-    console.log(todos);
+  var $list = $('.list');
+  var $info = $('.info');
+  $list.fadeOut(function() {
+    $list.children().remove();
+    $info.children().remove();
+    $.get('/todoSearch', {word: text}, function(todos) {
+      if(todos.length === 0) {
+        $info.append(makeInformation('err', '対象のToDoは見つかりません。'));
+        $info.fadeIn();
+      }
+      else {
+        $info.append(makeInformation('notice', todos.length + '件見つかりました。'));
+        $info.fadeIn();
+        $.each(todos, function(index, todo) {
+          $list.append('<p>' + todo.text + '</p>');
+        });
+        $list.fadeIn();
+      }
+    });
   });
 }
